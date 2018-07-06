55092e5628d1e654c6a3d926785743663e3625a2
Neil <Neilp1998@gmail.com>
2018-07-05 22:43:02 -0700

    started the process of in-memory storage

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <sstream>
+#include <vector>
+#include <iterator>
 
 enum MetaCommandResult {
     kMetaCommandSuccess,
@@ -9,6 +12,7 @@ enum MetaCommandResult {
 
 enum PrepareResult {
     kPrepareSuccess,
+    kPrepareSyntaxError,
     kPrepareUnrecognizedStatement
 };
 
@@ -17,9 +21,37 @@ enum StatementType {
     kStatementSelect,
 };
 
+namespace field_sizes { // byte size of fields and offsets for packing
+    const int kColumnIdSize = 4;
+    const int kColumnUsernameSize = 32;
+    const int kColumnEmailSize = 255;
+    const int kIdOffset = 0;
+    const int kUsernameOffset = kIdOffset + kColumnIdSize;
+    const int kEmailOffset = kUsernameOffset + kColumnUsernameSize;
+    const int kRowSize = kColumnIdSize + kColumnUsernameSize + kColumnEmailSize;
+}
+
+typedef struct Row_t {
+    int32_t id;
+    std::string username;
+    std::string email;
+
+    Row_t() : id(), username(), email() {}
+//        username.reserve(field_sizes::kColumnUsernameSize); // reserving max string sizes
+//        email.reserve(field_sizes::kColumnEmailSize);
+//    }
+} Row;
+
 typedef struct Statement_t {
     StatementType type;
+    Row row_to_insert; // only for insert statement
 } Statement;
+
+
+typedef struct CompactRow_t {
+//    unsigned char *row;
+    unsigned char row[field_sizes::kRowSize];
+} CompactRow;
 
 
 void print_prompt();
@@ -32,7 +64,29 @@ PrepareResult prepare_statement(std::string const &buf, Statement *statement);
 
 void execute_statement(Statement *statement);
 
+bool assign_insert_statement_args(std::string const &buf, Statement *statement);
+
+void serialize_row(Row* source, CompactRow *destination);
+
+void deserialize_row(CompactRow *source, CompactRow *destination);
+
 int main(int argc, char *argv[]) {
+
+    auto c = new char;
+    auto row = new CompactRow();
+    std::cout << field_sizes::kRowSize << std::endl;
+    std::cout << sizeof(row) << std::endl;
+    std::cout << row->row[10] << std::endl;
+    std::cout << c << std::endl;
+
+    auto count = 0;
+    for ( auto r : row->row) {
+        std::cout << r << std::endl;
+        count ++;
+    }
+
+    std::cout << count << std::endl;
+    std::cout << row->row[0] << std::endl;
 
     while (true) {
         print_prompt();
@@ -52,6 +106,9 @@ int main(int argc, char *argv[]) {
         switch (prepare_statement(input_buf, &statement)) {
             case (kPrepareSuccess):
                 break;
+            case (kPrepareSyntaxError):
+                std::cout << "syntax error in: " << input_buf << std::endl;
+                continue;
             case (kPrepareUnrecognizedStatement):
                 std::cout << "unrecognized keyword at the start of: " << input_buf << std::endl;
                 continue;
@@ -93,7 +150,12 @@ MetaCommandResult do_meta_command(std::string const &buf) {
 PrepareResult prepare_statement(std::string const &buf, Statement *statement) {
     if (buf.compare(0, 6, "insert") == 0) {
         statement->type = kStatementInsert;
-        return kPrepareSuccess;
+        bool success = assign_insert_statement_args(buf, statement);
+
+//        std::cout << statement->row_to_insert.id << statement->row_to_insert.username << statement->row_to_insert.email << std::endl;
+//        std::cout << success << std::endl;
+
+        return (success) ? kPrepareSuccess : kPrepareSyntaxError;
     }
     if (buf == "select") {
         statement->type = kStatementSelect;
@@ -112,4 +174,17 @@ void execute_statement(Statement *statement) {
             std::cout << "select stub" << std::endl;
             break;
     }
+}
+
+bool assign_insert_statement_args(std::string const &buf, Statement *statement) {
+    std::istringstream iss(buf);
+
+    iss.ignore(6); // ignore insert
+    iss >> statement->row_to_insert.id >> statement->row_to_insert.username >> statement->row_to_insert.email;
+
+//    std::cout << iss.str() << std::endl;
+//    std::cout << statement->row_to_insert.id << std::endl;
+//    std::cout << statement->row_to_insert.username << std::endl;
+//    std::cout << statement->row_to_insert.email << std::endl;
+    return iss.good();
 }
