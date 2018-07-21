705e81216f27bb217c93309ae6a5e2aa87867db2
Neil Parthasarthy <neilp1998@gmail.com>
2018-07-21 16:46:55 -0700

    incr save, needs debugging

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,11 @@ enum StatementType {
     kStatementSelect,
 };
 
+enum ExecuteResult_t {
+    kExecuteSuccess,
+    kExecuteTableFull,
+};
+
 namespace field_sizes { // byte size of fields and offsets for packing
     const int kColumnIdSize = 4;
     const int kColumnUsernameSize = 32;
@@ -29,6 +34,11 @@ namespace field_sizes { // byte size of fields and offsets for packing
     const int kUsernameOffset = kIdOffset + kColumnIdSize;
     const int kEmailOffset = kUsernameOffset + kColumnUsernameSize;
     const int kRowSize = kColumnIdSize + kColumnUsernameSize + kColumnEmailSize;
+    const int kRowSize = kColumnIdSize + kColumnUsernameSize + kColumnEmailSize;
+    const int kPageSize = 4096;
+    const int kTableMaxPages = 100;
+    const int kRowsPerPage = kPageSize / kRowSize;
+    const int kTableMaxRows = kRowsPerPage * kTableMaxPages;
 }
 
 typedef struct Row_t {
@@ -42,17 +52,23 @@ typedef struct Row_t {
 //    }
 } Row;
 
+typedef struct CompactRow_t {
+    char data[field_sizes::kRowSize];
+} CompactRow
+
 typedef struct Statement_t {
     StatementType type;
     Row row_to_insert; // only for insert statement
 } Statement;
 
+typedef struct Table_t {
+    Page *pages[field_sizes::kTableMaxPages]; 
+    int num_rows;
+} Table;
 
-typedef struct CompactRow_t {
-//    unsigned char *row;
-    unsigned char row[field_sizes::kRowSize];
-} CompactRow;
-
+typedef struct Page_t {
+    char *data[field_sizes::kPageSize];
+} Page
 
 void print_prompt();
 
@@ -66,14 +82,14 @@ void execute_statement(Statement *statement);
 
 bool assign_insert_statement_args(std::string const &buf, Statement *statement);
 
-void serialize_row(Row* source, CompactRow *destination);
+void serialize_row(Row* source, void *destination);
 
-void deserialize_row(CompactRow *source, CompactRow *destination);
+void deserialize_row(void *source, Row *destination);
+
+char* row_slot(Table *table, int row_num);
 
 int main(int argc, char *argv[]) {
-
-    auto c = new char;
-    auto row = new CompactRow();
+    Table * table = new table;
 
     while (true) {
         print_prompt();
@@ -101,8 +117,14 @@ int main(int argc, char *argv[]) {
                 continue;
         }
 
-        execute_statement(&statement);
-        std::cout << "executed " << std::endl;
+        switch (execute_statement(&statement)) {
+            case (kExecuteSuccess):
+                std::cout << "Executed" << std::endl;
+                break;
+            case (kExecuteTableFull):
+                std::cout << "Error: table full" << std::endl;
+                break;
+        }
 
         if (input_buf.empty()) break; // remove infinite loop warning
     }
@@ -152,14 +174,33 @@ PrepareResult prepare_statement(std::string const &buf, Statement *statement) {
     return kPrepareUnrecognizedStatement;
 }
 
-void execute_statement(Statement *statement) {
+ExecuteResult execute_insert(Statement *statement, Table *table) {
+    if (table->num_rows >= field:sizes::kTableMaxRows) {
+        return kExecuteTableFull;
+    }
+
+    Row *row_to_insert = &(statement->row_to_insert);
+
+    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    table->num_rows++;
+
+    return kExecuteSuccess;
+
+ExecuteResult execute_select(Statement *statement, Table *table) {
+    Row row;
+    for(int i = 0; i < table->num_rows; i++) {
+        deserialize_row(row_slot(table, i), &row);
+        print_row(&row);
+    }
+    return kExecuteSuccess;
+} 
+
+ExecuteResult execute_statement(Statement *statement, Table *table) {
     switch (statement->type) {
         case (kStatementInsert):
-            std::cout << "insert stub" << std::endl;
-            break;
+            return execute_insert(statemnt, table);
         case (kStatementSelect):
-            std::cout << "select stub" << std::endl;
-            break;
+            return execute_select(statement, table);
     }
 }
 
@@ -168,10 +209,37 @@ bool assign_insert_statement_args(std::string const &buf, Statement *statement) 
 
     iss.ignore(6); // ignore insert
     iss >> statement->row_to_insert.id >> statement->row_to_insert.username >> statement->row_to_insert.email;
-
+    
 //    std::cout << iss.str() << std::endl;
 //    std::cout << statement->row_to_insert.id << std::endl;
 //    std::cout << statement->row_to_insert.username << std::endl;
 //    std::cout << statement->row_to_insert.email << std::endl;
     return iss.good();
 }
+
+void serialize_row(Row* source, void *destination) {
+    std::memcpy(destination + field_sizes::kIdOffset, &(source->id), field_sizes::kColumnIdSize);
+    std::memcpy(destination + field_size::kUsernameOffset, &(source->username.c_str()), field_sizes::kColumnUsernameSize);
+    std::memcpy(destination + field_sizes::kEmailOffset, &(source->email.c_str()), field_sizes::kColumnEmailSize);
+}
+
+void deserialize_row(CompactRow *source, void *destination) {
+    std::memcpy(&(destination->id), source + field_sizes::kIdOffset, field_sizes::kColumnIdSize);
+    std::memcpy(&(destination->username), source + field_sizes::kUsernameOffset, field_sizes::kColumnUsernameSize);
+    std::memcpy(&(destination->email), source + field_sizes::kEmailOffset, field_sizes::kColumnEmailSize);
+}
+
+char* row_slot(Table *table, int row_num) {
+    int page_num = row_num / field_sizes::kRowsPerPage;
+    char *page = table->pages[page_num];
+    if(!page) {
+        page = new Page;
+        table->pages[page_num] = page;
+    }
+    int row_offset = row_num % field_size::kRowsPerPage;
+    int byte_offset = row_offset * field_sizes::kRowSize;
+    return page[byte_offset];
+}
+
+
+
